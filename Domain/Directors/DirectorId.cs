761428a04b2db59bd34d8141e1e345cd67bca97f
lonellylennon <vladislavackerman@gmail.com>
2024-12-08 23:28:47 +0200

    actors, directors

--- Domain/Directors/DirectorId.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Movies
-{
-    public record DirectorId(Guid Value)
-    {
-        public static DirectorId New() => new(Guid.NewGuid());
-        public static DirectorId Empty() => new(Guid.Empty);
-        public override string ToString() => Value.ToString();
-    }
-}
