eb31ee3900328087266df02dd1f59c7b019d159e
dAsHMeLnyK <daryna.melnyk@oa.edu.ua>
2024-12-10 20:08:23 +0200

    Domain Genres

--- Domain/Genres/GenreId.cs
+++
@@ -1,8 +0,0 @@
-namespace Domain.Movies;
-
-public record GenreId(Guid? Value)
-{
-    public static GenreId New() => new(Guid.NewGuid());
-    public static GenreId Empty() => new(Guid.Empty);
-    public override string ToString() => Value.ToString();
-}
--- Domain/Movies/MovieId.cs
+++
@@ -1,8 +0,0 @@
-namespace Domain.Movies;
-
-public record MovieId(Guid Value)
-{
-    public static MovieId New() => new(Guid.NewGuid());
-    public static MovieId Empty() => new(Guid.Empty);
-    public override string ToString() => Value.ToString();
-}
